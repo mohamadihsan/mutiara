f9a1608b2337ae8a3f55f534c3211f344a9ec7a1
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2017-12-13 11:02:53 +0700

    update database

--- admin/functions/koneksi.php
+++ admin/functions/koneksi.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "mekarsariDB";
+$dbname = "mutiara";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
