65c7e868b8b539b27778b34bd176aef37fbecdb0
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-06 10:14:16 +0700

    add update

--- admin/kepala-pemasaran/interfaces/jadwal-pengiriman.php
+++ admin/kepala-pemasaran/interfaces/jadwal-pengiriman.php
@@ -18,7 +18,7 @@
         	<div class="col-xs-12">
         		<div class="nav-tabs-custom">
 		            <ul class="nav nav-tabs pull-right">
-		              	<!-- <li class="active"><a href="" data-toggle="modal" data-target="#modalTambah" class="text-bold"><i class="fa fa-plus text-success"></i> Tambah</a></li> -->
+		              	<li class="active"><a href="?menu=jadwal-pengiriman&analisis" class="text-bold"><i class="fa fa-plus text-success"></i> Analisis Distribusi</a></li>
 		              	<li class="active"><a href="#tab_2-2" data-toggle="tab">Daftar Pemesanan</a></li>
 		            </ul>
 		            <div class="tab-content">
@@ -30,8 +30,8 @@
 				                  	<th>Nomor Faktur</th>
 				                  	<th>Pelanggan</th>
 				                  	<th>Tanggal Pengiriman</th>
+                                    <th>Alamat Pengiriman</th>
 				                  	<th class="text-center" width="5%">Status Pengiriman</th>
-				                  	<th></th>
 				                </tr>
 				                </thead>
 				                <tbody>
@@ -43,11 +43,15 @@
                                     	d.kendaraan,
                                     	d.nomor_faktur,
                                     	d.`status`,
-                                    	d.tanggal
+                                    	d.tanggal,
+                                    	p.nama,
+                                    	p.alamat
                                     FROM
                                     	distribusi d
+                                    LEFT JOIN pemesanan_produk pp ON pp.id = d.nomor_faktur
+                                    LEFT JOIN pelanggan p ON p.id = pp.pelanggan
                                     ORDER BY
-                                    	distribusi.tanggal DESC";
+                                    	d.tanggal DESC";
 									$result = mysqli_query($conn, $sql);
 
 									if (mysqli_num_rows($result) > 0) {
@@ -60,6 +64,8 @@
 									    	$tanggal = $row['tanggal'];
 									    	$kendaraan = $row['kendaraan'];
 									    	$status = $row['status'];
+									    	$nama = $row['nama'];
+									    	$alamat = $row['alamat'];
 
 									    	?>
 									        <tr>
@@ -67,6 +73,7 @@
 							                  	<td><?= strtoupper($row['nomor_faktur']) ?></td>
 							                  	<td><?= strtoupper($row['nama']) ?></td>
 							                  	<td><?= Tanggal($row['tanggal']).' ('.$jam_pemesanan.')' ?></td>
+                                                <td><?= $alamat ?></td>
 							                  	<td class="text-center">
 							                  		<?php
 							                  		if ($status=='B') {
@@ -75,10 +82,6 @@
 							                  			?><span class="label label-success">sudah dikirim</span><?php
 							                  		}
 							                  		?>
-							                  	</td>
-							                  	<td width="10%" class="text-center">
-							                  		<a  href="?menu=detail-pesanan&faktur=<?= $nomor_faktur ?>" class="btn btn-sm btn-default" title="Detail"><i class="fa fa-file-text-o"></i></a>
-							                  		<!-- <button class="btn btn-sm btn-success" title="Konfirmasi" data-toggle="modal" data-target="#modalKonfirmasi" onclick="return konfirmasi('<?= $id ?>')"><i class="fa fa-check"></i> Terima</button> -->
 							                  	</td>
 							                </tr>
 									        <?php
@@ -89,6 +92,36 @@
 
 				                </tbody>
 			              	</table>
+
+                            <?php
+                            if (isset($_GET['analisis'])) {
+                                $from = "Bandung";
+                                $to = "Bogor";
+
+                                $from = urlencode($from);
+                                $to = urlencode($to);
+
+                                $data = file_get_contents("http://maps.googleapis.com/maps/api/distancematrix/json?origins=$from&destinations=$to&language=id-ID&sensor=false");
+                                $data = json_decode($data);
+
+                                $time = 0;
+                                $distance = 0;
+
+                                foreach($data->rows[0]->elements as $road) {
+                                    $time += $road->duration->value;
+                                    $distance += $road->distance->value;
+                                }
+
+                                echo "Asal: ".$data->destination_addresses[0];
+
+                                echo "Tujuan: ".$data->origin_addresses[0];
+
+                                echo "Estimasi Waktu: ".$time." detik atau setara dengan " . gmdate('H', $time) . " jam " . gmdate('i', $time) . " menit";
+
+                                echo "Jarak: ".$distance." m atau " .floor($distance / 1000). " km";
+
+                            }?>
+
 			            </div>
 		              	<!-- /.tab-pane -->
 		            </div>
--- admin/staff-keuangan/interfaces/pemesanan-produk.php
+++ admin/staff-keuangan/interfaces/pemesanan-produk.php
@@ -2,10 +2,353 @@
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <div class="row">
-  	<div class="content text-center">
-  		<h3>Halaman ini masih dalam pengembangan</h3>
-  	</div>
-  </div>
+	<!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1 style="opacity: 1">
+        Pemesanan Produk
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="?menu=beranda"><i class="fa fa-home"></i> Beranda</a></li>
+        <li class="active">Pemesanan Produk </li>
+      </ol>
+    </section>
+	<!-- Main content -->
+    <section class="content">
+      	<div class="row">
+        	<div class="col-xs-12">
+        		<div class="nav-tabs-custom">
+		            <ul class="nav nav-tabs pull-right">
+		              	<li class="active"><a href="#tab_2-2" data-toggle="tab">Daftar Pemesanan</a></li>
+		            </ul>
+		            <div class="tab-content">
+		              	<div class="tab-pane active" id="tab_2-2">
+			            	<table id="example1" class="table table-striped table-bordered dt-responsive nowrap">
+				                <thead>
+				                <tr class="bg-success">
+				                  	<th width="5%">No</th>
+				                  	<th>Nomor Faktur</th>
+				                  	<th>Pelanggan</th>
+				                  	<th>Tanggal Pemesanan</th>
+				                  	<th>Tanggal Pembayaran</th>
+				                  	<th class="text-center">Metode Pembayaran</th>
+				                  	<th></th>
+				                </tr>
+				                </thead>
+				                <tbody>
+
+					                <?php
+					                $sql = "
+					                SELECT pemesanan_produk.id, pemesanan_produk.nomor_faktur, pemesanan_produk.tanggal_pembayaran, pemesanan_produk.metode_pembayaran, pemesanan_produk.tanggal, pemesanan_produk.status_pembayaran, pelanggan.nama, pelanggan.kode,  DATE_FORMAT(tanggal, '%H:%i:%s') AS jam_pemesanan, DATE_FORMAT(tanggal_pembayaran, '%H:%i:%s') AS jam_pembayaran, pegawai.nip, pegawai.nama_panggilan
+					                FROM pemesanan_produk
+					                LEFT JOIN pelanggan ON pemesanan_produk.pelanggan=pelanggan.id
+					                LEFT JOIN pegawai ON pemesanan_produk.pegawai=pegawai.id
+					                ORDER BY tanggal DESC";
+									$result = mysqli_query($conn, $sql);
+
+									if (mysqli_num_rows($result) > 0) {
+										$j=0;
+										$no = 1;
+									    // keluarkan data dalam variabel row
+									    while($row = mysqli_fetch_assoc($result)) {
+									    	$id = $row['id'];
+									    	$nomor_faktur = $row['nomor_faktur'];
+									    	$tanggal_pembayaran = $row['tanggal_pembayaran'];
+									    	$metode_pembayaran = $row['metode_pembayaran'];
+									    	$status_pembayaran = $row['status_pembayaran'];
+									    	$tanggal = $row['tanggal'];
+									    	$nama = $row['nama'];
+									    	$nama_panggilan = $row['nama_panggilan'];
+									    	$kode = $row['kode'];
+									    	$jam_pemesanan = $row['jam_pemesanan'];
+									    	$jam_pembayaran = $row['jam_pembayaran'];
+									    	$nip = $row['nip'];
+									    	$nama_panggilan = $row['nama_panggilan'];
+
+									    	$sql_status_po = "SELECT id FROM pemesanan_produk WHERE id='$id'";
+									    	$result_status_po=mysqli_query($conn,$sql_status_po);
+									    	$jml=mysqli_num_rows($result_status_po);
+									    	if ($jml==0) {
+									    		$status_po[$j] = 'belum disetujui';
+									    	}else{
+									    		$status_po[$j] = 'sudah disetujui';
+									    	}
+									    	?>
+									        <tr>
+							                  	<td><?= $no++; ?></td>
+							                  	<td><?= strtoupper($row['nomor_faktur']) ?></td>
+							                  	<td><?= strtoupper($row['nama']) ?></td>
+							                  	<td><?= Tanggal($row['tanggal']).' ('.$jam_pemesanan.')' ?></td>
+							                  	<td>
+							                  		<?php
+							                  		if($row['tanggal_pembayaran']==null){
+							                  			echo "-";
+						                  			}else{
+						                  				echo Tanggal($row['tanggal_pembayaran']).' ('.$jam_pembayaran.')';
+						                  			} ?>
+						                  		</td>
+							                  	<td class="text-center"><?= strtoupper($metode_pembayaran) ?></td>
+                                                <?php
+                                                if($row['tanggal_pembayaran']==null){ ?>
+                                                    <td width="10%" class="text-center">
+    							                  		<button class="btn btn-sm btn-success" title="Konfirmasi" data-toggle="modal" data-target="#modalKonfirmasi" onclick="return konfirmasi('<?= $id ?>', '<?= $nomor_faktur ?>')"><i class="fa fa-check"></i> Konfirmasi</button>
+    							                  	</td>
+                                                    <?php
+                                                }else{ ?>
+                                                    <td></td>
+                                                    <?php
+                                                }?>
+
+							                </tr>
+									        <?php
+									        $j++;
+									    }
+									}
+					                ?>
+
+				                </tbody>
+			              	</table>
+			            </div>
+		              	<!-- /.tab-pane -->
+		            </div>
+		            <!-- /.tab-content -->
+		        </div>
+		        <!-- nav-tabs-custom -->
+       		</div>
+        	<!-- /.col -->
+      	</div>
+      	<!-- /.row -->
+    </section>
+    <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+
+
+<!-- Modal Pembayaran' -->
+<div class="modal fade" id="modalKonfirmasi" role="dialog">
+    <div class="modal-dialog modal-sm">
+      	<div class="modal-content">
+        	<div class="modal-header">
+	          	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	          	<h4 class="modal-title"><i class="fa fa-check text-success"></i> Konfirmasi</h4>
+	        </div>
+	       <form action="" method="post" accept-charset="utf-8" class="form-horizontal">
+	        	<div class="modal-body">
+
+	        		<!-- ID  -->
+	        		<input type="hidden" name="id" id="id" class="form-control" placeholder="" required="">
+
+		          	<p>Konfirmasi pembayaran?</p>
+                    <input type="date" name="tanggal_pembayaran" value="<?= date('Y-m-d')?>" class="form-control">
+		        </div>
+		        <div class="modal-footer">
+		          	<button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+		          	<button type="submit" class="btn btn-success" name="konfirmasi_pembayaran">Konfirmasi</button>
+		        </div>
+	        </form>
+    	</div>
+	</div>
+</div>
+
+<script type="text/javascript">
+
+	function hapus(id){
+		$('.modal-body input[name=id]').val(id);
+	}
+
+	function konfirmasi(id, nomor_faktur){
+		$('.modal-body input[name=id]').val(id);
+		$('.modal-body input[name=nomor_faktur]').val(nomor_faktur);
+	}
+
+    //Date picker
+    $('#datepicker').datepicker({
+      autoclose: true
+    });
+</script>
+
+
+<!-- Pesan Proses Simpan  -->
+<?php
+// simpan konfirmasi pembayaran
+if (isset($_POST['konfirmasi_pembayaran'])) {
+    $id = $_POST['id'];
+    $status_pembayaran = 'S';
+    $tanggal_pembayaran = $_POST['tanggal_pembayaran'];
+    $sql = "UPDATE pemesanan_produk SET tanggal_pembayaran='$tanggal_pembayaran', status_pembayaran='$status_pembayaran' WHERE id='$id'";
+    if (mysqli_query($conn,$sql)) {
+
+        // cek kapasitas
+        $sql = "SELECT SUM(jumlah) as kapasitas FROM detail_penjualan_produk WHERE faktur='$id'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $kapasitas = $row['kapasitas'];
+
+        // pilih kendaraan
+        $sql = "SELECT id FROM kendaraan WHERE kapasitas >= '$kapasitas' ORDER BY kapasitas ASC LIMIT 1";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $kendaraan = $row['id'];
+
+        // penentuan tanggal
+        $sql = "SELECT tanggal_pembayaran FROM pemesanan_produk WHERE id='$id'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $tanggal_pembayaran = $row['tanggal_pembayaran'];
+        $jam_pembayaran = date('H', strtotime($tanggal_pembayaran));
+        if ($jam_pembayaran > 16) {
+            $tanggal = date('Y-m-d', strtotime('+1 days', strtotime($tanggal_pembayaran)));
+        }else{
+            $tanggal = $tanggal_pembayaran;
+        }
+
+        $status = '0';
+        // insert distribusi
+        $sql = "INSERT INTO distribusi(nomor_faktur, kendaraan, tanggal, status) VALUES('$id', '$kendaraan', '$tanggal', '$status')";
+        mysqli_query($conn, $sql);
+
+        $_SESSION['simpan_berhasil'] = true;
+    }else{
+        $_SESSION['simpan_gagal'] = true;
+    }
+
+}
+
+
+if (isset($_SESSION['simpan_berhasil'])) {
+	?>
+    <script type="text/javascript">
+	  $(function(){
+	    $.gritter.add({
+	        // (string | mandatory) the heading of the notification
+	        title: 'Sukses!',
+	        // (string | mandatory) the text inside the notification
+	        text: 'Data berhasil disimpan.',
+	        // (string | optional) the image to display on the left
+	        image: 'assets/images/berhasil.png',
+	        // (bool | optional) if you want it to fade out on its own or just sit there
+	        sticky: false,
+	        // (int | optional) the time you want it to be alive for before fading out
+	        time: ''
+	      });
+	  });
+	</script>
+    <?php
+    /*unset session*/
+    unset($_SESSION['simpan_berhasil']);
+}else if (isset($_SESSION['simpan_gagal'])) {
+	?>
+    <script type="text/javascript">
+	  $(function(){
+	    $.gritter.add({
+	        // (string | mandatory) the heading of the notification
+	        title: 'Gagal!',
+	        // (string | mandatory) the text inside the notification
+	        text: 'Error : Terjadi kesalahan saat menyimpan data.',
+	        // (string | optional) the image to display on the left
+	        image: 'assets/images/gagal.png',
+	        // (bool | optional) if you want it to fade out on its own or just sit there
+	        sticky: false,
+	        // (int | optional) the time you want it to be alive for before fading out
+	        time: ''
+	      });
+	  });
+	</script>
+    <?php
+    /*unset session*/
+    unset($_SESSION['simpan_gagal']);
+}
+?>
+
+<!-- Pesan Proses Ubah  -->
+<?php
+if (isset($_SESSION['ubah_berhasil'])) {
+	?>
+    <script type="text/javascript">
+	  $(function(){
+	    $.gritter.add({
+	        // (string | mandatory) the heading of the notification
+	        title: 'Sukses!',
+	        // (string | mandatory) the text inside the notification
+	        text: 'Data berhasil diperbaharui.',
+	        // (string | optional) the image to display on the left
+	        image: 'assets/images/berhasil.png',
+	        // (bool | optional) if you want it to fade out on its own or just sit there
+	        sticky: false,
+	        // (int | optional) the time you want it to be alive for before fading out
+	        time: ''
+	      });
+	  });
+	</script>
+    <?php
+    /*unset session*/
+    unset($_SESSION['ubah_berhasil']);
+}else if (isset($_SESSION['ubah_gagal'])) {
+	?>
+    <script type="text/javascript">
+	  $(function(){
+	    $.gritter.add({
+	        // (string | mandatory) the heading of the notification
+	        title: 'Gagal!',
+	        // (string | mandatory) the text inside the notification
+	        text: 'Error : Terjadi kesalahan saat memperbaharui data.',
+	        // (string | optional) the image to display on the left
+	        image: 'assets/images/gagal.png',
+	        // (bool | optional) if you want it to fade out on its own or just sit there
+	        sticky: false,
+	        // (int | optional) the time you want it to be alive for before fading out
+	        time: ''
+	      });
+	  });
+	</script>
+    <?php
+    /*unset session*/
+    unset($_SESSION['ubah_gagal']);
+}
+?>
+
+<!-- Pesan Proses Hapus  -->
+<?php
+if (isset($_SESSION['hapus_berhasil'])) {
+	?>
+    <script type="text/javascript">
+	  $(function(){
+	    $.gritter.add({
+	        // (string | mandatory) the heading of the notification
+	        title: 'Sukses!',
+	        // (string | mandatory) the text inside the notification
+	        text: 'Data berhasil dihapus.',
+	        // (string | optional) the image to display on the left
+	        image: 'assets/images/berhasil.png',
+	        // (bool | optional) if you want it to fade out on its own or just sit there
+	        sticky: false,
+	        // (int | optional) the time you want it to be alive for before fading out
+	        time: ''
+	      });
+	  });
+	</script>
+    <?php
+    /*unset session*/
+    unset($_SESSION['hapus_berhasil']);
+}else if (isset($_SESSION['hapus_gagal'])) {
+	?>
+    <script type="text/javascript">
+	  $(function(){
+	    $.gritter.add({
+	        // (string | mandatory) the heading of the notification
+	        title: 'Gagal!',
+	        // (string | mandatory) the text inside the notification
+	        text: 'Error: Terjadi kesalahan saat menghapus data.',
+	        // (string | optional) the image to display on the left
+	        image: 'assets/images/gagal.png',
+	        // (bool | optional) if you want it to fade out on its own or just sit there
+	        sticky: false,
+	        // (int | optional) the time you want it to be alive for before fading out
+	        time: ''
+	      });
+	  });
+	</script>
+    <?php
+    /*unset session*/
+    unset($_SESSION['hapus_gagal']);
+}
+?>
