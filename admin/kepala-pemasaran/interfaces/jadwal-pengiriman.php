534fa5df11339e8ffe075f35f5069d759377aeac
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-02-15 09:23:57 +0700

    update analisis ditribusi

--- admin/kepala-pemasaran/interfaces/jadwal-pengiriman.php
+++ admin/kepala-pemasaran/interfaces/jadwal-pengiriman.php
@@ -18,111 +18,226 @@
         	<div class="col-xs-12">
         		<div class="nav-tabs-custom">
 		            <ul class="nav nav-tabs pull-right">
-		              	<li class="active"><a href="" data-toggle="modal" data-target="#modalAnalisis" class="text-bold"><i class="fa fa-plus text-success"></i> Analisis Distribusi</a></li>
+                        <?php
+                        if (isset($_GET['tanggal'])) {
+                            ?>
+                            <li class="active"><a href="?menu=jadwal-pengiriman" class="text-bold"><i class="fa fa-arrow-left text-success"></i> Kembali</a></li>
+    		              	<?php
+                        }else{
+                            ?>
+                            <li class="active"><a href="" data-toggle="modal" data-target="#modalAnalisis" class="text-bold"><i class="fa fa-plus text-success"></i> Analisis Distribusi</a></li>
+    		              	<?php
+                        }
+                        ?>
 		              	<li class="active"><a href="#tab_2-2" data-toggle="tab">Daftar Pemesanan</a></li>
 		            </ul>
 		            <div class="tab-content">
 		              	<div class="tab-pane active" id="tab_2-2">
-			            	<table id="example1" class="table table-striped table-bordered dt-responsive nowrap">
-				                <thead>
-				                <tr class="bg-success">
-				                  	<th width="5%">No</th>
-				                  	<th>Nomor Faktur</th>
-				                  	<th>Pelanggan</th>
-				                  	<th>Tanggal Pengiriman</th>
-				                  	<th>Kendaraan</th>
-                                    <th>Alamat Pengiriman</th>
-				                  	<th class="text-center" width="5%">Status Pengiriman</th>
-				                </tr>
-				                </thead>
-				                <tbody>
-
-					                <?php
-					                $sql = "
-                                    SELECT
-                                    	d.id,
-                                    	k.plat_nomor,
-                                    	pp.nomor_faktur,
-                                    	d.`status`,
-                                    	d.tanggal,
-                                    	p.nama,
-                                    	p.alamat
-                                    FROM
-                                    	distribusi d
-                                    LEFT JOIN pemesanan_produk pp ON pp.id = d.nomor_faktur
-                                    LEFT JOIN pelanggan p ON p.id = pp.pelanggan
-                                    LEFT JOIN kendaraan k ON k.id=d.kendaraan
-                                    ORDER BY
-                                    	d.tanggal DESC";
-									$result = mysqli_query($conn, $sql);
-
-									if (mysqli_num_rows($result) > 0) {
-										$j=0;
-										$no = 1;
-									    // keluarkan data dalam variabel row
-									    while($row = mysqli_fetch_assoc($result)) {
-									    	$id = $row['id'];
-									    	$nomor_faktur = $row['nomor_faktur'];
-									    	$tanggal = $row['tanggal'];
-									    	$plat_nomor = $row['plat_nomor'];
-									    	$status = $row['status'];
-									    	$nama = $row['nama'];
-									    	$alamat = $row['alamat'];
-
-									    	?>
-									        <tr>
-							                  	<td><?= $no++; ?></td>
-							                  	<td><?= strtoupper($row['nomor_faktur']) ?></td>
-							                  	<td><?= strtoupper($row['nama']) ?></td>
-							                  	<td><?= Tanggal($row['tanggal']).' ('.$jam_pemesanan.')' ?></td>
-                                                <td><?= $plat_nomor ?></td>
-                                                <td><?= $alamat ?></td>
-							                  	<td class="text-center">
-							                  		<?php
-							                  		if ($status=='B' OR $status == '0') {
-							                  			?><span class="label label-warning">belum dikirim</span><?php
-							                  		}else{
-							                  			?><span class="label label-success">sudah dikirim</span><?php
-							                  		}
-							                  		?>
-							                  	</td>
-							                </tr>
-									        <?php
-									        $j++;
-									    }
-									}
-					                ?>
-
-				                </tbody>
-			              	</table>
 
                             <?php
                             if (isset($_GET['analisis'])) {
-                                $from = "Bandung";
-                                $to = "Bogor";
 
-                                $from = urlencode($from);
-                                $to = urlencode($to);
 
-                                $data = file_get_contents("http://maps.googleapis.com/maps/api/distancematrix/json?origins=$from&destinations=$to&language=id-ID&sensor=false");
-                                $data = json_decode($data);
+                                // echo "Estimasi Waktu: ".$time." detik atau setara dengan " . gmdate('H', $time) . " jam " . gmdate('i', $time) . " menit";
+                                //
+                                // echo "Jarak: ".$distance." m atau " .floor($distance / 1000). " km";
 
-                                $time = 0;
-                                $distance = 0;
+                                $tanggal_analisis = $_GET['tanggal'];
 
-                                foreach($data->rows[0]->elements as $road) {
-                                    $time += $road->duration->value;
-                                    $distance += $road->distance->value;
+                                $sql = "SELECT
+                                	d.id,
+                                	pp.nomor_faktur,
+                                	pl.nama,
+                                	pl.provinsi,
+                                	pl.kabupaten,
+                                	pl.kecamatan,
+                                	pl.alamat,
+                                	k.plat_nomor,
+                                	pg.nama_lengkap,
+                                	DATE_FORMAT(d.tanggal, '%Y-%m-%d') AS tanggal_pengiriman,
+                                	d.status as status_pengiriman
+                                FROM
+                                	distribusi d
+                                LEFT JOIN pemesanan_produk pp ON pp.id = d.nomor_faktur
+                                LEFT JOIN pelanggan pl ON pl.id = pp.pelanggan
+                                LEFT JOIN kendaraan k ON k.id = d.kendaraan
+                                LEFT JOIN pegawai pg ON pg.id = k.supir
+                                WHERE
+                                	DATE_FORMAT(d.tanggal, '%Y-%m-%d') = '$tanggal_analisis'
+                                ORDER BY k.plat_nomor ";
+
+                                $result = mysqli_query($conn, $sql);
+                                if (mysqli_num_rows($result)>0) {
+                                    ?>
+
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>Faktur</th>
+                                                <th>Kendaraan</th>
+                                                <th>Supir</th>
+                                                <th width="40%">Alamat</th>
+                                                <th>Jarak</th>
+                                                <th>Estimasi</th>
+                                                <th width="10%">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <?php
+                                            $i=0;
+                                            $alamat_perusahaan = "UNIKOM Bandung";
+                                            $susunan = $alamat_perusahaan;
+                                            while ($row=mysqli_fetch_assoc($result)) {
+                                                $id = $row['id'];
+                                                $nomor_faktur = $row['nomor_faktur'];
+                                                $nama = $row['nama'];
+                                                $provinsi = $row['provinsi'];
+                                                $kabupaten = $row['kabupaten'];
+                                                $kecamatan = $row['kecamatan'];
+                                                $alamat = $row['alamat'];
+                                                $plat_nomor[$i] = $row['plat_nomor'];
+                                                $nama_supir = $row['nama_lengkap'];
+                                                $tanggal_pengiriman = $row['tanggal_pengiriman'];
+                                                $status_pengiriman = $row['status_pengiriman'];
+
+                                                if ($i==0) {
+                                                    $cara_pengiriman = '<div class="label label-success">dikirim</div>';
+                                                }else{
+                                                    if ($plat_nomor[$i] == $plat_nomor[$i-1]) {
+                                                        $cara_pengiriman = '<div class="label label-success">dikirim bersamaan</div>';
+                                                    }else{
+                                                        $cara_pengiriman = '<div class="label label-success">dikirim terpisah</div>';
+                                                    }
+                                                }
+
+                                                $from = $alamat_perusahaan;
+                                                $to = $alamat;
+
+                                                if ($i != 0) {
+                                                    $to = $alamat.' '.$provinsi;
+                                                }
+
+                                                $from = urlencode($from);
+                                                $to = urlencode($to);
+
+                                                $data = file_get_contents("http://maps.googleapis.com/maps/api/distancematrix/json?origins=$from&destinations=$to&language=id-ID&sensor=false");
+                                                $data = json_decode($data);
+
+                                                $time = 0;
+                                                $distance[$i] = 0;
+
+                                                foreach($data->rows[0]->elements as $road) {
+                                                    $time += $road->duration->value;
+                                                    $distance[$i] += $road->distance->value;
+                                                }
+
+                                                if ($distance[$i] > $distance[$i-1]) {
+                                                    $susunan = $susunan.'  =============>>>  '.$alamat;
+                                                }
+                                                ?>
+
+                                                <tr>
+                                                    <td><?= $nomor_faktur ?></td>
+                                                    <td><?= $plat_nomor[$i] ?></td>
+                                                    <td><?= $nama_supir ?></td>
+                                                    <td><?= $alamat.' '.$provinsi.' '.$kabupaten.' '.$kecamatan ?></td>
+                                                    <td><?= floor($distance[$i] / 1000). " km" ?></td>
+                                                    <td><?= gmdate('H', $time) . " jam " . gmdate('i', $time) . " menit" ?></td>
+                                                    <td><?= $cara_pengiriman ?></td>
+                                                </tr>
+
+                                                <?php
+                                                $i++;
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                    <br />
+                                    <?php
+                                    // echo "Asal: ".$data->destination_addresses[0];
+                                    // echo "<b>AWAL KEBERANGKATAN : </b><br>".$data->origin_addresses[0].'<br /><br />';
+                                    echo "<b>ANALISIS DISTRIBUSI TANGGAL: </b><br>".Tanggal($tanggal_analisis).'<br /><br />';
+                                    echo "<b>AWAL KEBERANGKATAN : </b><br>".$alamat_perusahaan.'<br /><br />';
+                                    echo "<b>SUSUNAN PENGIRIMAN : </b><br>".$susunan.'<br /><br />';
+                                }else{
+                                    echo "Data tidak ditemukan";
                                 }
+                            }else{
+                                ?>
+                                <table id="example1" class="table table-striped table-bordered dt-responsive nowrap">
+    				                <thead>
+    				                <tr class="bg-success">
+    				                  	<th width="5%">No</th>
+    				                  	<th>Nomor Faktur</th>
+    				                  	<th>Pelanggan</th>
+    				                  	<th>Tanggal Pengiriman</th>
+    				                  	<th>Kendaraan</th>
+                                        <th>Alamat Pengiriman</th>
+    				                  	<th class="text-center" width="5%">Status Pengiriman</th>
+    				                </tr>
+    				                </thead>
+    				                <tbody>
 
-                                echo "Asal: ".$data->destination_addresses[0];
+    					                <?php
+    					                $sql = "
+                                        SELECT
+                                        	d.id,
+                                        	k.plat_nomor,
+                                        	pp.nomor_faktur,
+                                        	d.`status`,
+                                        	d.tanggal,
+                                        	p.nama,
+                                        	p.alamat
+                                        FROM
+                                        	distribusi d
+                                        LEFT JOIN pemesanan_produk pp ON pp.id = d.nomor_faktur
+                                        LEFT JOIN pelanggan p ON p.id = pp.pelanggan
+                                        LEFT JOIN kendaraan k ON k.id=d.kendaraan
+                                        ORDER BY
+                                        	d.tanggal DESC";
+    									$result = mysqli_query($conn, $sql);
 
-                                echo "Tujuan: ".$data->origin_addresses[0];
+    									if (mysqli_num_rows($result) > 0) {
+    										$j=0;
+    										$no = 1;
+    									    // keluarkan data dalam variabel row
+    									    while($row = mysqli_fetch_assoc($result)) {
+    									    	$id = $row['id'];
+    									    	$nomor_faktur = $row['nomor_faktur'];
+    									    	$tanggal = $row['tanggal'];
+    									    	$plat_nomor = $row['plat_nomor'];
+    									    	$status = $row['status'];
+    									    	$nama = $row['nama'];
+    									    	$alamat = $row['alamat'];
 
-                                echo "Estimasi Waktu: ".$time." detik atau setara dengan " . gmdate('H', $time) . " jam " . gmdate('i', $time) . " menit";
+    									    	?>
+    									        <tr>
+    							                  	<td><?= $no++; ?></td>
+    							                  	<td><?= strtoupper($row['nomor_faktur']) ?></td>
+    							                  	<td><?= strtoupper($row['nama']) ?></td>
+    							                  	<td><?= Tanggal($row['tanggal']).' ('.$jam_pemesanan.')' ?></td>
+                                                    <td><?= $plat_nomor ?></td>
+                                                    <td><?= $alamat ?></td>
+    							                  	<td class="text-center">
+    							                  		<?php
+    							                  		if ($status=='B' OR $status == '0') {
+    							                  			?><span class="label label-warning">belum dikirim</span><?php
+    							                  		}else{
+    							                  			?><span class="label label-success">sudah dikirim</span><?php
+    							                  		}
+    							                  		?>
+    							                  	</td>
+    							                </tr>
+    									        <?php
+    									        $j++;
+    									    }
+    									}
+    					                ?>
 
-                                echo "Jarak: ".$distance." m atau " .floor($distance / 1000). " km";
-
+    				                </tbody>
+    			              	</table>
+                                <?php
                             }?>
 
 			            </div>
